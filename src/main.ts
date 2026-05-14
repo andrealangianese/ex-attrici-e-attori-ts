961dc4c9c4d9afdde018539629668edeb3f333e0
andrealangianese <andre.langia.01@gmail.com>
2026-05-14 11:33:06 +0200

    controllo proprietà con funzione isactress

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,19 @@ type Actress = Person & {
   awards: string,
   nationality: 'American' | 'British' | 'Australian' | 'Israeli - American' | 'South African' | 'French' | 'Indian' | 'Israeli' | 'Spanish' | 'South Korean' | 'Chinese'
 }
+
+function isActress(data: unknown): data is Actress {
+  return (
+    // dati deve essere vero
+    data &&
+    typeof data === 'object' &&
+    data !== null &&
+    // controllo su tutte le proprietà
+    'id' in data && typeof data.id === 'number' &&
+    'name' in data && typeof data.name === 'string' &&
+    'birth_year' in data && typeof data.birth_year === 'number' &&
+    'death_year' in data && typeof data.death_year === 'number' &&
+    'biography' in data && typeof data.biography === 'string' &&
+    'image' in data && typeof data.image === 'string' &&
+  )
+}
